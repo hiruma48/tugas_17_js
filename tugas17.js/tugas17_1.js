6198012798ea443384e5f12420a7f9c0e7f9507a
tri ariyanto <triyanto129@gmail.com>
2023-05-18 14:39:30 +0700

    tugas 17 js
    mengumpulkan tugas

---
+++ tugas17.js/tugas17_1.js
@@ -0,0 +1,10 @@
+function lowupp (){
+var huruf1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk. ";
+console.log(huruf1.toLowerCase());
+console.log(huruf1.toUpperCase());
+var huruf2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(huruf2.toLowerCase());
+console.log(huruf2.toUpperCase());
+}
+
+lowupp();
---
+++ tugas17.js/tugas17_2.js
@@ -0,0 +1,12 @@
+function charat(){
+    var kata = "Lorem ";
+    console.log(
+        kata.charCodeAt(0),"-",
+        kata.charCodeAt(1),"-",
+        kata.charCodeAt(2),"-",
+        kata.charCodeAt(3),"-",
+        kata.charCodeAt(4),"-",
+        kata.charCodeAt(5)
+        );
+}
+charat();
